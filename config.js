83e3509a8084990b1ad5c1c735f85904e88e012a
cofm dubaii <96689200+cofm34@users.noreply.github.com>
2025-03-04 22:10:46 +0800

    UPLOAD

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const nameGirl = 'Énn';
 const giftUrl = 'http://cofm.store';
-const eventName = 'CHÚC 8/3';
+const eventName = 'ÉNN 🧸';
 const titleCard = 'Gửi Énn';
 const contentCard = 'Chúc tình yêu của anh 8/3 tràn ngập niềm vui và những nụ cười. Mong điều đẹp nhất sẽ đến với em trong hôm nay và cả những ngày sau';
 
