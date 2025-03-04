2869f55f15f19ee4d9b2ab2679a5827254064af8
cofm dubaii <96689200+cofm34@users.noreply.github.com>
2025-03-04 18:51:21 +0800

    thay anh

--- config.js
+++ config.js
@@ -4,6 +4,6 @@ const eventName = 'CHÚC 8/3';
 const titleCard = 'Gửi Énn';
 const contentCard = 'Chúc tình yêu của anh 8/3 tràn ngập niềm vui và những nụ cười. Mong điều đẹp nhất sẽ đến với em trong hôm nay và cả những ngày sau';
 
-const giftImage = 'hot-girl.png';
+const giftImage = 'enn.jpg';
 const base64 = '';
 const giftImageBase64 = "data:image/png;base64, " + base64;
